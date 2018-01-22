dcdbedf2c3ed1127b88a92179fce4dab3ac9ff77
vicus <vasile.pop@gmail.com>
2018-01-23 00:26:45 +0200

    added migration for books

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mai Vasile
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = DB::table('books')->get()->sortBy('title');
+
+    return view('books', compact('books'));
 });
